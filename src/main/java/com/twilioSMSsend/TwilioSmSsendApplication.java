62f9ffda2c4ec6cf0b79285a1b94a22a64aa933d
iamrakeshit <iamrakeshit273@gmail.com>
2024-02-23 20:32:40 +0530

    127:twilio SMS send created

---
+++ src/main/java/com/twilioSMSsend/TwilioSmSsendApplication.java
@@ -0,0 +1,13 @@
+package com.twilioSMSsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioSmSsendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioSmSsendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twilioSMSsend/TwilioSmSsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twilioSMSsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioSmSsendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
